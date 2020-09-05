6d6c51ed41cf3c61e223177da1bb350c57569ef5
Tessa Seiders <tessa.seiders@gmail.com>
2020-09-05 15:36:27 -0500

    updated to use only 1 file and trying to get render to work

--- jojo-inventory-app/src/App.js
+++ jojo-inventory-app/src/App.js
@@ -1,54 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './styles/App.css';
-import tile from './images/tile.jpg';
 import AddItem from './components/AddItem'
+import tile from './images/tile.jpg';
+
+const app = {
+  itemName: 'Coffee',
+  itemQuantity: 6,
+  items: []
+}
+
+const onItemFormSubmit = (e) => {
+  e.preventDefault()
+  const getItem = e.target.elements.item.value
+  console.log(getItem);
+  
+  if(getItem){
+      app.items.push(getItem)
+      e.target.elements.getItem.value = ''
+      //render()
+  }
+}
+const appRoot = document.getElementById('root')
+const template = (
+  <div className="App">
+  <div className="App-header">
+      <h1>Food Inventory App</h1>
+      <p>{app.items.length > 0 ? 'Here are your items': 'No items'}</p>
+      <p>{app.items.length}</p>
+      <form onSubmit={onItemFormSubmit}>
+          <input type="text" name="item"/>
+          <button>Add Item</button>
+      </form>
+      <ol>
+          <li>{app.itemName}</li>
+          <li>{app.itemQuantity}</li>
+          <br></br>
+          <img src={tile} alt="generic food" />
+      </ol>
+  </div>
+</div>
+)
 
 export default function App() {
-  
-  const app = {
-    itemName: 'Coffee',
-    itemQuantity: 6,
-    items: []
-  }
-  const onItemFormSubmit = (e) => {
-    e.preventDefault()
-    const item = e.target.elements.item.value
-    console.log(item);
-    
-    if(item){
-        app.items.push(item)
-        e.target.elements.getItem.value = ''
-    }
-  }
-  const appRoot = document.getElementById('root')
-
-  const render = () => {
-    const template = (
-      <div className="App-header">
-        <h1>Food Inventory App</h1>
-        <p>{app.items.length > 0 ? 'Here are your items': 'No items'}</p>
-        <p>{app.items.length}</p>
-        <form onSubmit={onItemFormSubmit}>
-            <input type="text" name="item"/>
-            <button>Add Item</button>
-        </form>
-        <ol>
-            <li>{app.itemName}</li>
-            <li>{app.itemQuantity}</li>
-            <br></br>
-            <img src={tile} alt="generic food" />
-        </ol>
-      </div>
-    )
-    ReactDOM.render(template, appRoot)
-  }
-
-render()
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    ReactDOM.render(template, appRoot)
   );
 }
--- jojo-inventory-app/src/components/AddItem.js
+++ jojo-inventory-app/src/components/AddItem.js
@@ -15,26 +15,28 @@ const onItemFormSubmit = (e) => {
     if(getItem){
         app.items.push(getItem)
         e.target.elements.getItem.value = ''
+        //render()
     }
 }
-
 
 export default class AddItem extends React.Component{
     render() {
         return (
-            <div>
-                <form onSubmit={onItemFormSubmit}>
-                    <input type="text" name="item"/>
-                    <button>Add Item</button>
-                </form>
-                <p>{app.items.length}</p>
-                <ul>
-                    <li>{app.itemName}</li>
-                    <li>{app.itemQuantity}</li>
-                    <br></br>
-                    <img src={tile} alt="generic food" />
-                </ul>
-            </div>
+            <div className="App-header">
+            <h1>Food Inventory App</h1>
+            <p>{app.items.length > 0 ? 'Here are your items': 'No items'}</p>
+            <p>{app.items.length}</p>
+            <form onSubmit={onItemFormSubmit}>
+                <input type="text" name="item"/>
+                <button>Add Item</button>
+            </form>
+            <ol>
+                <li>{app.itemName}</li>
+                <li>{app.itemQuantity}</li>
+                <br></br>
+                <img src={tile} alt="generic food" />
+            </ol>
+        </div>
         )
     }
 }
